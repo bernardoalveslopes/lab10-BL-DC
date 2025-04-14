d9ea9434842a7aa861f2804448a37f7438039d4c
bernardoalveslopes <bernardoluis.asl@gmail.com>
2025-04-14 14:03:41 -0300

    Update calculator.py
    started to define the functions

--- calculator.py
+++ calculator.py
@@ -5,7 +5,23 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+import math
+
+
 def add(a, b): 
-    pass
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    return b/a
+    
+def exponent(a,b):
+    return a**b
+
 
 
