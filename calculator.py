f6eb0911f2a8df86d009f963f7856ab7d76e3d50
culve <darryl.culver@ufl.edu>
2025-04-14 22:39:48 -0400

    renamed functions to match required names

--- calculator.py
+++ calculator.py
@@ -29,10 +29,10 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
-def multiply(a, b):
+def mul(a, b):
     return a * b
 
-def divide(a, b):
+def div(a, b):
     if b == 0:
         raise ZeroDivisionError("Cannot divide by zero")
     return a / b
@@ -42,5 +42,5 @@ def logarithm(a, b):
         raise ValueError("Invalid base or argument for logarithm")
     return math.log(b, a)
 
-def exponent(a, b):
+def exp(a, b):
     return a ** b
