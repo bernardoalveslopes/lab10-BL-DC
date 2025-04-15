2741c84008c3491e037c59b045078bd1ca1899e6
culve <darryl.culver@ufl.edu>
2025-04-14 22:35:07 -0400

    renamed functions to match required names

--- calculator.py
+++ calculator.py
@@ -10,24 +10,37 @@ One function per operation, in order.
 # First example
 import math
 
+# Partner info
+# https://github.com/bernardoalveslopes/lab10-BL-DC
+# Partner 1: Bernardo Lopes
+# Partner 2: Darryl Culver
+
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot take square root of negative number")
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
 def add(a, b):
     return a + b
 
-def sub(a, b):
+def subtract(a, b):
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     return a * b
 
-def div(a, b):
+def divide(a, b):
     if b == 0:
         raise ZeroDivisionError("Cannot divide by zero")
     return a / b
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("Invalid base or argument for logarithm")
     return math.log(b, a)
 
-def exp(a, b):
+def exponent(a, b):
     return a ** b
